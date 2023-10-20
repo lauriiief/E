9ab1c92dc59a87a71007228ded2d44314ce204a7
laura <lauraespanyol95@gmail.com>
2023-10-20 08:39:44 +0200

    Added a space after de doble colon

--- Object Methods/exercise.js
+++ Object Methods/exercise.js
@@ -9,7 +9,7 @@ let entries = Object.entries(person);
 
 for (let entry of entries) {
     let [key, val] = entry;
-    console.log(key + ':' + val);
+    console.log(key + ': ' + val);
 }
 
 // Print values of person using Object.keys
@@ -18,5 +18,5 @@ let keys = Object.keys(person);
 let values = Object.values(person);
 
 for (let i = 0; i < values.length; i+= 1) {
-  console.log(keys[i] + ':' + values[i]);
+  console.log(keys[i] + ': ' + values[i]);
 }
