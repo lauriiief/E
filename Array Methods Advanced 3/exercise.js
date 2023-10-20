c26e360ebc3482fa347408b3f58ad461bf8c3841
laura <lauraespanyol95@gmail.com>
2023-10-20 11:28:50 +0200

    array method

---
+++ Array Methods Advanced 3/exercise.js
@@ -0,0 +1,23 @@
+function calculateAverageAge(persons) {
+  let ages = persons.map((el) => el.age);
+  let average = Math.round(ages.reduce((a,b) => a+b,0) / ages.length);
+  return average;
+  
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
