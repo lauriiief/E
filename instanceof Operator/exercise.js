4af4d671de34a4de8ef8542ec66ec9027d2b3fb3
laura <lauraespanyol95@gmail.com>
2023-10-27 11:54:11 +0200

    first try

--- instanceof Operator/exercise.js
+++ instanceof Operator/exercise.js
@@ -1,29 +1,52 @@
-class Square {
+class Allshapes {
+
+}
+
+
+class Square extends Allshapes{
   constructor(side) {
+    super()
     this.side = side;
   }
 }
 
-class Rectangle {
+class Rectangle extends Allshapes{
   constructor(width, height) {
+    super()
     this.width = width;
     this.height = height;
   }
 }
 
-class Circle {
+class Circle extends Allshapes{
   constructor(radius) {
+    super()
     this.radius = radius;
   }
 }
 
-class AreaCalculator {
-  //...
+class AreaCalculator extends Allshapes{
+  static calculate(value) {
+    if(value === Circle.class) {
+      return 3.14 * (Circle.radius * Circle.radius);
+    } else if(Square.class) {
+      return Square.side * Square.side;
+    } else if(Rectangle.class){
+      return Rectangle.width * Rectangle.height;
+    } else {
+      return "The class is not recognized";
+    }
+}
 }
 
 const square = new Square(4);
 const rectangle = new Rectangle(4, 2);
 const circle = new Circle(5);
+
+console.log(square instanceof Allshapes)
+console.log(rectangle instanceof Allshapes)
+console.log(circle instanceof Allshapes)
+
 
 console.log(AreaCalculator.calculate(square));
 console.log(AreaCalculator.calculate(rectangle));
