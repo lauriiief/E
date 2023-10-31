01153bf2889e8a014d9e32f81bb95ff726dbdd1d
laura <lauraespanyol95@gmail.com>
2023-10-31 09:17:08 +0100

    rest exercises

--- Rest parameters and spread syntax 1/exercise.js
+++ Rest parameters and spread syntax 1/exercise.js
@@ -1,4 +1,4 @@
 let numberStore = [0, 1, 2];
-let newNumber = 3;
-numberStore.push(newNumber);
+
+numberStore = [...numberStore, 3];
 console.log(numberStore);
--- Rest parameters and spread syntax 4/exercise.js
+++ Rest parameters and spread syntax 4/exercise.js
@@ -1,6 +1,13 @@
-function sum(num1, num2, num3) {
-  return num1 + num2 + num3;
+//function sum(array) {
+//  return array.reduce((a,b) => a + b);
+//}
+
+//const numbers = [1, 2, 3];
+//console.log(sum(numbers));
+
+function sum([...arr]) {
+  return arr.reduce((a, b) => a + b);
 }
 
 const numbers = [1, 2, 3];
-console.log(sum(numbers[0], numbers[1], numbers[2]));
+console.log(sum(numbers));
